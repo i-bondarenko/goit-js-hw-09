7f396634a10598ff859bd33322c365044b3ac9ca
i-bondarenko <104758542+i-bondarenko@users.noreply.github.com>
2022-10-18 09:21:39 +0000

    Deploying to gh-pages from @ i-bondarenko/goit-js-hw-09@c0a3e979844b2359698f30a0cb382c8396162c04 🚀

---
+++ 01-color-switcher.7a91cb86.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]"),bodyEl:document.querySelector("body")};let e=null;t.btnStop.disabled=!0,t.btnStart.addEventListener("click",(function(o){o.target.disabled=!0,t.btnStop.removeAttribute("disabled"),e=setInterval((()=>{t.bodyEl.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.btnStop.addEventListener("click",(function(o){t.btnStart.removeAttribute("disabled"),o.target.disabled=!0,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.7a91cb86.js.map
